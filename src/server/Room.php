82e2d76a7c2a7996cab2b3041f402e505fc817b2
dominic <dominic@docoli.de>
2018-10-21 16:22:03 +0200

    Basic room and player handling.

---
+++ src/server/Room.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace TickTackToe\server;
+
+
+class Room
+{
+    private $player;
+    private $id;
+    private $playerCount;
+
+    /**
+     * Room constructor.
+     * @param $roomId
+     *
+     * @return void
+     */
+    public function __construct($roomId)
+    {
+        $this->id = $roomId;
+    }
+
+    /**
+     * Join room and count player.
+     *
+     * @param $player
+     *
+     * @return void
+     */
+    public function join($player): void
+    {
+        $this->playerCount++;
+        $this->player = $player;
+    }
+
+    /**
+     * Returns and player resource id.
+     *
+     * @return mixed
+     */
+    public function getPlayer(): int
+    {
+        return $this->player;
+    }
+
+    /**
+     * Returns the room id.
+     *
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Return the current connected players.
+     *
+     * @return mixed
+     */
+    public function getPlayerCount(): int
+    {
+        return $this->playerCount;
+    }
+
+
+}
